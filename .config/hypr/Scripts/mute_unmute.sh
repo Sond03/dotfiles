37cd169f5e639fe11b7293a0f5aa2710dec8b495
sondre <sondret321@gmail.com>
2026-06-28 20:26:32 +0200

    changed mute script

--- .config/hypr/Scripts/mute_unmute.sh
+++ .config/hypr/Scripts/mute_unmute.sh
@@ -3,10 +3,10 @@
 vol_check=$(wpctl get-volume @DEFAULT_SOURCE@ | awk '{print $3}')
 
 if  [ "$vol_check" = "[MUTED]" ]; then
-    paplay -d alsa_output.usb-Apple_Inc._AirPods_Max_USB_Audio_FJPVQXGXDR-01.analog-stereo /home/sond/.config/hypr/Scripts/sounds/unmute.mp3
+    paplay /home/sond/.config/hypr/Scripts/sounds/unmute.mp3
     notify-send --icon=/home/sond/.config/mako/icons/unmuted.png "Discord" "microphone unmuted"
 else
-    paplay -d alsa_output.usb-Apple_Inc._AirPods_Max_USB_Audio_FJPVQXGXDR-01.analog-stereo /home/sond/.config/hypr/Scripts/sounds/mute.mp3
+    paplay /home/sond/.config/hypr/Scripts/sounds/mute.mp3
     notify-send --icon=/home/sond/.config/mako/icons/mute.png "Discord" "microphone muted" 
 fi
 
